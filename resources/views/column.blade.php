8b021250ece627e9298142417fdac5ffcce1de6f
Ryan Chandler <ryangjchandler@gmail.com>
2024-03-25 23:31:13 +0000

    feature: support custom Filament colours

--- resources/views/column.blade.php
+++ resources/views/column.blade.php
@@ -1,14 +1,9 @@
 @php
-$evaluatedColor = $getColor();
-$color = match ($evaluatedColor) {
-    'primary' => 'bg-primary-600',
-    'secondary' => 'bg-secondary-600',
-    'danger' => 'bg-danger-600',
-    'success' => 'bg-success-600',
-    'warning' => 'bg-warning-600',
-    default => $evaluatedColor
-};
-
+$color = $getColor();
+$barStyles = \Filament\Support\get_color_css_variables(
+    $color,
+    shades: [600],
+);
 $progress = $getProgress();
 $poll = $getPoll();
 @endphp
@@ -21,10 +16,7 @@ $poll = $getPoll();
 >
     <div class="flex items-center space-x-4 px-4">
         <div class="bg-gray-200 rounded-full h-2.5 dark:bg-gray-600 w-full">
-            <div @class([
-                'h-2.5 rounded-full',
-                $color,
-            ]) style="width: {{ min($progress, 100) }}%"></div>
+            <div style="{{ $barStyles }}; width: {{ min($progress, 100) }}%" class="h-2.5 rounded-full bg-custom-600"></div>
         </div>
 
         <span class="text-sm text-gray-700 dark:text-gray-200">{{ $progress }}%</span>
